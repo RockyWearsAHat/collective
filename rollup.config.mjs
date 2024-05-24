88571f9d5b08917cb057af432c12afd771156068
Rocky <alexwaldmann2004@gmail.com>
2024-05-24 02:43:48 -0600

    Maybe pt 2

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,12 +1,48 @@
 import typescript from "@rollup/plugin-typescript";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import gzipPlugin from "rollup-plugin-gzip";
 
 import { glob } from "glob";
 import { extname, sep } from "node:path";
 import { fileURLToPath } from "node:url";
 import { builtinModules } from "node:module";
 import { copyFile } from "./plugins/copyFile.js";
+import MagicString from "magic-string";
+
+function rewriteGzipImports() {
+  // match dynamic imports with .js but not .gz.js
+  const patternDImport = new RegExp(/"\.\/([\w\-.]+)\.js(?<!\.gz\.js)"/, "mg");
+  return {
+    name: "rewriteImports",
+    renderChunk(code) {
+      const magicString = new MagicString(code);
+      let hasReplacements = false;
+      let match;
+      let start;
+      let end;
+      let replacement;
+
+      function replaceImport() {
+        hasReplacements = true;
+        start = match.index + 3;
+        end = start + match[1].length;
+        replacement = String(match[1].replace(match[1], match[1] + ".gz"));
+        magicString.overwrite(start, end, replacement);
+      }
+
+      // work against dynamic imports
+      while ((match = patternDImport.exec(code))) {
+        replaceImport();
+      }
+
+      if (!hasReplacements) {
+        return null;
+      }
+      return { code: magicString.toString() };
+    }
+  };
+}
 
 export default [
   {
@@ -46,6 +82,8 @@ export default [
       resolve({ preferBuiltins: true, jsnext: true, main: true }),
       commonjs({ ignoreDynamicRequires: true, ignore: builtinModules }),
       copyFile("robots.txt", "build/robots.txt"),
+      gzipPlugin(),
+      rewriteGzipImports()
     ]
   }
 ];
