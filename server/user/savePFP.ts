da2b3715dfec7706e1ddea3b20439f4daf1784da
Rocky <alexwaldmann2004@gmail.com>
2024-05-24 12:32:14 -0600

    Removed sharp

--- server/user/savePFP.ts
+++ server/user/savePFP.ts
@@ -3,7 +3,7 @@ import multer, { memoryStorage } from "multer";
 import { withAuth } from "../auth/masterAuthRouter";
 import { uploadToS3 } from "../helpers/s3";
 import { User } from "../../db/models/user";
-import sharp from "sharp";
+// import sharp from "sharp";
 
 export const savePFPRouter: Router = Router();
 
@@ -16,14 +16,14 @@ const savePFP = async (req: Request, res: Response) => {
   if (!file || !req.session.user?._id)
     return res.status(400).json({ message: "No file uploaded" });
 
-  const convertedImageBuffer = await sharp(file.buffer)
-    .toFormat("jpg")
-    .toBuffer();
+  // const convertedImageBuffer = await Sharp(file.buffer)
+  //   .toFormat("jpg")
+  //   .toBuffer();
 
-  //Load image with new converted data
-  file.buffer = convertedImageBuffer;
-  file.originalname = file.originalname.replace(/\.[^/.]+$/, ".jpg");
-  file.mimetype = "image/jpeg";
+  // //Load image with new converted data
+  // file.buffer = convertedImageBuffer;
+  // file.originalname = file.originalname.replace(/\.[^/.]+$/, ".jpg");
+  // file.mimetype = "image/jpeg";
 
   const { error, key } = await uploadToS3(file, req.session.user._id);
   if (error) return res.status(500).json({ message: (error as Error).message });
