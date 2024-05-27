754d866d726e2f12c53772e9bc240dae66954153
Rocky <alexwaldmann2004@gmail.com>
2024-05-26 18:28:58 -0600

    Testing RS256

--- server/tokens/jwt.ts
+++ server/tokens/jwt.ts
@@ -2,16 +2,17 @@ import jwt from "jsonwebtoken";
 // import fs from "fs";
 import { IUser } from "../../db/models/user";
 
-const privateKey = "placeholderForNowRS256IsComingSoon";
-// fs.readFileSync(`${process.cwd()}/server/tokens/RS256.key`) ||
-// process.env.RS256KEY;
-// const publicKey =
-//   fs.readFileSync(`${process.cwd()}/server/tokens/RS256.key.pub`) ||
-//   process.env.RS256KEYPUB;
+const privateKey = process.env.RS256KEY;
+const publicKey = process.env.RS256KEYPUB;
+
+if (!privateKey || !publicKey) {
+  console.error("No RS256 public/private key found in environment variables");
+  process.exit();
+}
 
 const signToken = async (user?: IUser): Promise<string | null> => {
   let JWT: string = jwt.sign(user?.toJSON() || {}, privateKey, {
-    // algorithm: "RS256",
+    algorithm: "RS256",
     expiresIn: "2h"
   });
 
@@ -26,8 +27,7 @@ export interface IToken {
 
 const validateToken = async (token: string): Promise<boolean> => {
   try {
-    // jwt.verify(token, publicKey);
-    jwt.verify(token, privateKey);
+    jwt.verify(token, publicKey);
     return true;
   } catch (error) {
     if (error instanceof jwt.TokenExpiredError) {
